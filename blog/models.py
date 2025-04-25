a26bb95f879f6e969956b0a0f99669ffe12bca1f
Mubassir Habib <66573095+Mubassir1820@users.noreply.github.com>
2025-04-25 16:41:50 +0600

    model for CustomUser and blogs with url

---
+++ blog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.conf import settings
+from django.utils.text import slugify
+from datetime import datetime
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    slug = models.SlugField(unique=True, blank=True) # new field for slug
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='blogs')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = f"{slugify(self.title)}_{hash(datetime.now)}"
+        super().save(*args, **kwargs)
